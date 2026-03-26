75928292f0d4f8fc2ec709db41ceb6f83aadf9d1
consi <consi@consi.com>
2026-03-26 11:53:51 +0100

    tty control soon

--- src/menu.py
+++ src/menu.py
@@ -1,7 +1,9 @@
 from src.colors import Colors, Format
 from pydantic import BaseModel, Field
 from os import listdir
-from sys import stderr
+from termios import tcgetattr, tcsetattr, TCSADRAIN
+from tty import setraw
+from sys import stderr, stdin
 from typing import Generator, List
 
 
@@ -41,3 +43,40 @@ class Menu(BaseModel):
         self.form.centered(head)
         body: List = list(maps for maps in self.maps.get_maps())
         self.form.centered(self.form.listing(body))
+        navigate.first_draw = True
+        while (True):
+            key = get_key()
+
+            if (key == '\x1b[A'):
+                selected = (selected - 1) % len(body)
+            elif (key == '\x1b[B'):
+                selected = (selected + 1) % len(body)
+            elif (key == '\x03'):
+                break
+            navigate(body, selected)
+
+
+    def get_key(self):
+        fd = stdin.fileno()
+        old = tcgetattr(fd)
+        try:
+            setraw(fd)
+            char = stdin.read(1)
+            if (char == '\x1b'):
+                char += stdin.read(2)
+            return (char)
+        finally:
+            tcsetattr(fd, TCSADRAIN, old)
+
+    def navigate(items, selection):
+        if (navigate.first_draw):
+            navigate.first_draw = False
+        else:
+            stdout.write('\x1b[{len(items)}A')
+
+        for i, item in enumerate(items):
+            if (i == selected):
+                marker = 'x'
+            else:
+                marker = ' '
+            self.form.centered(self.form.listing(item))
